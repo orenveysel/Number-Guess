cf5c52786c623504f57d0078aa950e02a7f417d3
Veysel ÖREN <88139807+orenveysel@users.noreply.github.com>
2022-07-22 01:52:04 +0300

    Update Number-Guess.py

--- Number-Guess.py
+++ Number-Guess.py
@@ -1,39 +1,39 @@
 '''
-    1-100 arasında rasgele üretilecek bir sayıyı aşağı yukarı ifadeleri ile
-    buldurmaya çalışın.
-    **  "random modülü" için "python random" şeklinde arama yapın.
-    **  100 üzerinden puanlama yapın. Her soru 20 puan.
-    **  Hak bilgisini kullanıcıdan alın ve her soru belirtilen can sayısı
-        üzerinden hesaplansın.
+    A number that will be generated randomly between 1-100 and 4, with roughly
+    try to find it.
+    ** Search "python random" for "random module."
+    ** Rating through 100. Every question is 20 points.
+    ** Get the Rights from the user and each question is the specified number of lives
+    calculate over.
 '''
 
 import random
 
 x = random.randint(1,100)
 
-print("***** SAYI TAHMİN OYUNUNA HOŞGELDİNİZ *****")
+print("***** WELCOME TO THE NUMBER GUESSING GAME *****")
 
-hak = int(input("Kaç canla oynamak istersin: "))
-puanlama = 100/hak
+live = int(input("How many lives do you want to start with?: "))
+point = 100/live
 
 i = 0
 score = 100
-while i < hak:
-    a = int(input("Tahmin et: "))
+while i < live:
+    a = int(input("Take a guess: "))
     
-    if i + 1 == hak:
-        print("Hakkınız bitti!")
-        print(f"Sayı {x} idi :( ")
-        print("Puanınız 0")
+    if i + 1 == live:
+        print("Your lives are over!")
+        print(f"The number was {x}:( ")
+        print("Your score is 0")
         break
     if a == x:
-        print(f"Tebrikler, sayıyı {i+1}. denemede buldunuz!")
-        print(f"Puanınız {score:1.4}")
+        print(f"Congratulations, you found the number in the {i+1}. attempt!")
+        print(f"Your score is {score:1.4}")
         break
     elif a < x:
-        print("Yukarı!!")
+        print("Up!!")
     elif a > x:
-        print("Aşağı!!")   
+        print("Down!!")   
     i += 1
-    score -= puanlama
+    score -= point
     
